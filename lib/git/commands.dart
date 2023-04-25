8b7592cb02fe25d1f6463ad024253eb646313451
Justin Inoa <justin@odespra.com>
2023-04-25 19:07:59 -0400

    commands

---
+++ lib/git/commands.dart
@@ -0,0 +1,9 @@
+// Upload - - - - - - - - - - - - - - - - - - - - //
+
+// git add .
+// git commit -m 'message'
+// git push -u origin master
+
+// Get Data - - - - - - - - - - - - - - - - - - - //
+
+// git pull -u origin master
