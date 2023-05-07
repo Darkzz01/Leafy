cb4ed7aa6f88b856122800380f7add2569043b30
Justin Inoa <justin@odespra.com>
2023-05-07 18:10:51 -0400

    admin 1.0.0

--- lib/pages/admin/admin.dart
+++ lib/pages/admin/admin.dart
@@ -27,69 +27,66 @@ class _AdminState extends State<Admin> {
                       color: Colors.blueGrey,
                     ),
                   ),
-                  Expanded(
-                    flex: 3,
-                    child: Container(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          DataTable(
-                            columnSpacing: size.width * 0.08,
-                            border: TableBorder.all(),
-                            columns: const [
-                              DataColumn(
-                                label: Text('ID'),
-                              ),
-                              DataColumn(
-                                label: Text('Name'),
-                              ),
-                              DataColumn(
-                                label: Text('Last Name'),
-                              ),
-                              DataColumn(
-                                label: Text('Email'),
-                              ),
-                              DataColumn(
-                                label: Text('Country'),
-                              ),
-                              DataColumn(
-                                label: Text('State'),
-                              ),
-                              DataColumn(
-                                label: Text('City'),
-                              ),
-                            ],
-                            rows: const [
-                              DataRow(
-                                cells: [
-                                  DataCell(
-                                    Text('1'),
-                                  ),
-                                  DataCell(
-                                    Text('Justin'),
-                                  ),
-                                  DataCell(
-                                    Text('@'),
-                                  ),
-                                  DataCell(
-                                    Text('@'),
-                                  ),
-                                  DataCell(
-                                    Text('@'),
-                                  ),
-                                  DataCell(
-                                    Text('@'),
-                                  ),
-                                  DataCell(
-                                    Text('@'),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        DataTable(
+                          columnSpacing: size.width * 0.08,
+                          border: TableBorder.all(),
+                          columns: const [
+                            DataColumn(
+                              label: Text('ID'),
+                            ),
+                            DataColumn(
+                              label: Text('Name'),
+                            ),
+                            DataColumn(
+                              label: Text('Last Name'),
+                            ),
+                            DataColumn(
+                              label: Text('Email'),
+                            ),
+                            DataColumn(
+                              label: Text('Country'),
+                            ),
+                            DataColumn(
+                              label: Text('State'),
+                            ),
+                            DataColumn(
+                              label: Text('City'),
+                            ),
+                          ],
+                          rows: const [
+                            DataRow(
+                              cells: [
+                                DataCell(
+                                  Text('1'),
+                                ),
+                                DataCell(
+                                  Text('Justin'),
+                                ),
+                                DataCell(
+                                  Text('@'),
+                                ),
+                                DataCell(
+                                  Text('@'),
+                                ),
+                                DataCell(
+                                  Text('@'),
+                                ),
+                                DataCell(
+                                  Text('@'),
+                                ),
+                                DataCell(
+                                  Text('@'),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                   ),
                 ],
